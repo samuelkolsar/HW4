b4f95c0f4a24221ac0d1a07f7afbb84036f7d9e4
karlaine <karl.laine@ut.ee>
2024-12-12 16:42:13 +0200

    Add your password by yourself

--- server/database.js
+++ server/database.js
@@ -3,7 +3,7 @@ const Pool = require('pg').Pool;
 // this code will work and a table will be created if you have already created the "testWad" database.
 const pool = new Pool({
     user: "postgres",
-    password: "KarlLaine28", // Enter your password here
+    password: "", // Enter your password here
     database: "testWad", //Try to use the same name for your database
     host: "localhost",
     port: "5433"
